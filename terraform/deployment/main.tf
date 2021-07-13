4932fc771dd097777f186bc0470e537eaef58aa5
anderp2 <87140754+anderp2@users.noreply.github.com>
2021-07-13 12:57:29 -0400

    Update main.tf

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -137,7 +137,35 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 }
 
-output "connection_string" {
-  value = azurerm_cosmosdb_account.db.connection_strings
-  sensitive = true
+resrouce "azure_keyvault" "db_keyvault"{
+   name                       = "db_keyvault"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "delete",
+      "purge",
+      "recover"
+    ]
+  } 
+}
+
+resource "azure_keyvault_secret" "db_connection_strings"{
+  name         = "db_connection_strings"
+  value        = azurerm_cosmosdb_account.db.connection_strings
+  key_vault_id = azurerm_key_vault.example.id
 }
