7edfeec529702ce558b27ed10f7789a55b73ccba
anderp2 <87140754+anderp2@users.noreply.github.com>
2021-07-14 13:12:10 -0400

    Update main.tf

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -82,14 +82,14 @@ resource "azurerm_virtual_network" "vnet1" {
   name                = var.vnet1
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet1" {
   name           = var.subnet1
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefix = "10.0.0.0/24"
+  address_prefix = "10.1.0.0/24"
 }
 
 resource "azurerm_network_interface" "nic-cc-dev" {
